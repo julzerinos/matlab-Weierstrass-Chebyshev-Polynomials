ae26454176dba38077902e2b534e17e32c857d58
Julian Szachowicz <julzerinos@gmail.com>
2019-05-21 22:09:22 +0200

    Update files

--- Weierstrass.m
+++ Weierstrass.m
@@ -7,30 +7,21 @@ function [x,k]=Weierstrass(x0, p, tol, max_iter)
 % tol : tolerance
 % max_iter : maximum number of iterations
 
+n = max(size(p))
+
 dx=tol+1;
 k=0; x=[];
 
-dp=polyder(p);
-while abs(dx)> tol && k<= max_iter 
+while norm(dx)> tol && k<= max_iter 
 
-    
-    
-    
-%     w=polyval(p,x0);
-%     if abs(w)<=tol
-%         x=x0; return;
-%     end
-%     
-%     dw=polyval(dp,x0);
-%     if dw==0
-%         disp(' You divide by zero! ');
-%         return;
-%     end
-%     dx=w/dw;
-%     x=x0-dx;
-%     k=k+1;
-%     x0=x;
+    for i = i:n
+        dx(i) = WeierstrassPolynomial(n, x0(i), p)/product1
+    end
+    x=x0-dx;
+    k=k+1;
+    x0=x;
 end
 
 end
     
+% TD product1 function to calculate products
--- test.m
+++ test.m
@@ -2,12 +2,14 @@ n = 4;
 p = zeros(1, n + 1);
 xa = zeros(1, n);
 for it = 1:n
-    p(1, it) = 1/(2^(n-it));
+    p(1, it) = 1/(2^(n-it)); % change to be random
     xa(1, it) = rand + 1i*rand;
 end
 
+% TD examples
+
 x = 0;
 w = WeierstrassPolynomial(4, x, p);
+p
 xa
-roots(p)
-[x, k] = Weierstrass(xa, p, 1e-20, 100)
+[x, k] = Weierstrass(xa, p, 1e-6, 100)
