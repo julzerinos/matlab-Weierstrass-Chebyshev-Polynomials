0b5ffb2bbad80f5573f6551118bc95f66a67eb5c
Julian Szachowicz <julzerinos@gmail.com>
2019-05-27 10:07:59 +0200

    Create test which doesn't work

--- test.m
+++ test.m
@@ -1,15 +1,14 @@
-n = 1;
-
-p = rand(n + 1, 1);
-p(1) = 1/(2^(n-1));
-
-xa = rand(n, 1);
-
 % 
 % % TD examples
 % 
 
-x = 0;
-w = WeierstrassPolynomial(x, p)
-p
-% [x, k] = Weierstrass(xa, p, 1e-6, 100)
+% Test 1
+
+n = 3;
+p = [0.25, 2, 3, -1];
+xi = rand(n, 1);
+
+[xf, k] = Weierstrass(xi, p, 1e-6, 100);
+
+xf, k
+% Test 2
