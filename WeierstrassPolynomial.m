ac861bbdf72d417b68edf7df7728e60b7caad6c3
Kornelos <kornel1902@gmail.com>
2019-05-27 09:35:41 +0200

    fixed weierstrasspoly

--- WeierstrassPolynomial.m
+++ WeierstrassPolynomial.m
@@ -1,14 +1,13 @@
 function w=WeierstrassPolynomial(x,p)
 
-% TD change to be more efficient
-n = max(size(p));
+n = max(size(p))-1;
 t(1) = x;
 t(2) = 2*x^2-1;
-w=t(1)*p(1)+t(2)+p(2);
+w=t(1)*p(n)+t(2)*p(n-1)+p(n+1);
 for i=3:n
-    % w = w + p(1, i) * Chebyshev(n + 1 - i, x);
+
      t(i) = 2*x*t(i-1)-t(i-2); 
-     w = w + p(i) * t(i);
+     w = w + p(n-i) * t(i);
 end
 
 end
