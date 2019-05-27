2b85583dfd3b89b4b81fa41d71d5f3f907ce4756
Kornelos <kornel1902@gmail.com>
2019-05-27 09:43:46 +0200

    tests in waierstrass

--- WeierstrassPolynomial.m
+++ WeierstrassPolynomial.m
@@ -1,9 +1,20 @@
 function w=WeierstrassPolynomial(x,p)
 
 n = max(size(p))-1;
+if(n == 0)
+    error("n can't be 0")
+end
 t(1) = x;
+if(n == 1)
+     w = t(1)*p(n);
+     return
+end
 t(2) = 2*x^2-1;
-w=t(1)*p(n)+t(2)*p(n-1)+p(n+1);
+if(n == 2)
+    w = t(1)*p(n)+t(2)*p(n-1);
+    return
+end
+w = t(1)*p(n)+t(2)*p(n-1)+p(n+1);
 for i=3:n
 
      t(i) = 2*x*t(i-1)-t(i-2); 
