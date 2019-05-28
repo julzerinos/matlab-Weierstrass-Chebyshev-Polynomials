76ee5f2b94cca15f423bb98cef149829f0cb6666
Julian Szachowicz <julzerinos@gmail.com>
2019-05-28 20:21:52 +0200

    Fix example.2

--- example_2.m
+++ example_2.m
@@ -3,13 +3,12 @@ p = [1/(2^6), zeros(1, n)];
 for i = 1:n
     xi(i) = cos((2*i-1)*pi/(2*n));
 end
-    
-[xf, k] = Weierstrass(xi, p, 1e-6, 1);
 
-fplot(@(x) WeierstrassPolynomial(x, p), [-10, 10])
+[xf, k] = Weierstrass(xi, p, 1e-6, 100);
+fplot(@(x) WeierstrassPolynomial(x, p), [-2, 2])
 grid on
 for i = 1:n
-    hold on 
-    w = WeierstrassPolynomial(xf(i), p);
-    line([xf(i) xf(i)],[w - 100, w + 100]);
+    hold on
+    w = WeierstrassPolynomial(xf(1, i), p);
+    line([xf(1, i) xf(1, i)],[w - 100, w + 100]);
 end
